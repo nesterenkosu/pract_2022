8ccb9675d3881a3d8d825fae7237c33bf69d9473
nesterenkosu <62102496+nesterenkosu@users.noreply.github.com>
2022-06-28 11:41:01 +0500

    Add files via upload

---
+++ my_site.ru/www/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Наш первый пример</title>
+		<script type="text/javascript" src="jquery-3.6.0.min.js"></script>
+		<script type="text/javascript">
+			$(function(){
+				//Главная функция jquery-программы
+				$("#btn_go").click(function(){
+					//Функция, которая будет вызвана 
+					//при нажатии на кнопку
+					alert("Привет, "+$("#txt_username").val()+"!");
+				});
+			});
+		</script>
+	</head>
+	<body>
+		<?
+			for($i=0;$i<10;$i++) {
+				echo "Hello, PHP!<br/>";
+			}
+		?>	
+		<b>Hello</b>, <i>world</i>! <br/>
+		<hr/>
+		Введите ваше имя: <br/>
+		<input id="txt_username" type="text"/><br/>
+		Выберите ваш город:<br/>
+		<select>
+		  <option>Челябинск</option>
+		  <option>Москва</option>
+		</select><br/>
+		<input id="btn_go" type="button" value="Нажать!"/>
+	</body>
+</html>
